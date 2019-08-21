d66290334fc16cd90ef9bb9baf66daa5c8653d49
Lyndon <lyndon.zhai@hotmail.com>
2019-08-21 21:23:09 +0900

    final version

--- number.py
+++ number.py
@@ -1,6 +1,10 @@
 import random
+start = input('Please enter start random number: ')
+end = input('Please enter end random number: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 #count = count + 1
