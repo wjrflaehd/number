7d317114cc7caa17269fdd47358698eac6512c10
Lyndon <lyndon.zhai@hotmail.com>
2019-08-21 21:18:41 +0900

    add count

--- number.py
+++ number.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 #count = count + 1
 	num = input('Please input number: ')
 	num = int(num)
 	if num == r:
 		print('Success')
+		print('This is the', count, 'time')
 		break
 	elif num > r:
 		print('Too big')
 	elif num < r:
 		print('Too small')
+	print('This is the', count, 'time')
